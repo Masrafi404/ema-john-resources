4ebea77898755f14866863f4e3ed1fb79df86b53
Masrafi404 <mdmasrafi902@gmail.com>
2023-03-30 09:40:40 +0600

    show dat a from cart

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-    //reduce way1
-    const reduce = (previous, current) => previous + current.price;
-    const totalPrice = cart.reduce(reduce, 0)
-    //reduce way2
     let totalShipping = 0;
+    let quantity = 0;
+    let totalPrice = 0;
+    //reduce way1
+    // const reduce = (previous, current) => previous + current.price;
+    // const totalPrice = cart.reduce(reduce, 0)
+    //reduce way2
     for (const product of cart) {
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+
     }
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart-container'>
             <h4>Order Summery</h4>
-            <p>Selected items: {cart.length}
+            <p>Selected items: {quantity}
+
             </p>
             <p>Total Price: ${totalPrice}</p>
-            <p>Total Shiping Charge: {totalShipping}</p>
+            <p>Total Shipping Charge: {totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
             <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
